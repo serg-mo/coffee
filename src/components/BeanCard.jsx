fe6342376dcbf4752e21d1a85d126019d22a0c29
Sergey Morozov <morozov.sergey@gmail.com>
2025-03-07 21:32:50 -0500

    sku

--- src/components/BeanCard.jsx
+++ src/components/BeanCard.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 
-export default function BeanCard({ specifications, description }) {
+export default function BeanCard({ sku, specifications, description }) {
   return (
     <div className="text-sm w-full space-y-6">
-      <div className="flex flex-row">
+      <div className="flex flex-row text-gray-600">
         <div className="w-1/2 mr-4">
+          <div className="flex w-full m-auto justify-between">
+            <span>SKU</span>
+            <span className="font-medium">{sku}</span>
+          </div>
+
           {Object.entries(specifications).map(([key, value]) => (
             <div className="flex w-full m-auto justify-between" key={key}>
-              <span className="text-gray-600">{key}</span>
+              <span>{key}</span>
               <span className="font-medium">{value}</span>
             </div>
           ))}
         </div>
 
-        <p className="w-1/2 text-sm text-gray-600 leading-relaxed">
+        <p className="w-1/2 text-sm leading-relaxed">
           {description}
         </p>
       </div>
